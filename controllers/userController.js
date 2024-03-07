d869594f8c660a838a7754a332deb27bdb7b179f
TylerHartwell <thartwell37@gmail.com>
2024-03-07 11:12:36 -0800

    start setup of controllers

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+// exports.login = (req, res){
+
+// }
+// exports.logout = (req, res){
+
+// }
+exports.register = (req, res) => {
+  res.send("wow")
+}
+exports.home = (req, res) => {
+  res.render("home-guest")
+}
